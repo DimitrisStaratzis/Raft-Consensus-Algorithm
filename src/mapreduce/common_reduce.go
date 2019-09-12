73d9bb719a6a19d12410c10302c062bf350f7fbf
dimitrisstaratzis <jstaratzi@icloud.com>
2019-09-11 22:27:14 -0400

    initial

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -54,6 +54,11 @@ func doReduce(
 			}
 			DecodedKeyValue[kv.Key] = append(DecodedKeyValue[kv.Key], kv.Value)
 		}
+		fi, err := indFileData.Stat()
+		if err != nil {
+			// Could not obtain stat, handle error
+		}
+		fmt.Printf("The file is %d bytes long", fi.Size())
 		indFileData.Close()
 	}
 	mergeFile, err := os.OpenFile(mergeName(jobName, reduceTaskNumber), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -65,11 +70,7 @@ func doReduce(
 		fmt.Println(values)
 		//skata
 		enc.Encode(KeyValue{key, reduceF(key, values)})
-		fi, err := mergeFile.Stat()
-		if err != nil {
-			// Could not obtain stat, handle error
-		}
-		fmt.Println("The file is %d bytes long", fi.Size())
+
 		mergeFile.Close()
 
 	}
