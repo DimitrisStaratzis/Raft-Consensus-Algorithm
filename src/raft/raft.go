10bc9774ec59575f3446fef61af0d5c9f448c5e9
dimitrisstaratzis <jstaratzi@icloud.com>
2019-09-25 17:39:53 -0400

    initial request vote

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -304,12 +304,12 @@ func (rf *Raft) startServer() {
 
 func startElection(rf *Raft) {
 	fmt.Println("ELECTION STARTS")
-	rf.mu.Lock()
+	//rf.mu.Lock()
 	rf.state = 1
 	rf.votesFor = rf.me //vote myself
 	rf.lastElectionStarted = time.Now().UnixNano()
 	rf.currentTerm += 1 //increase current term
-	rf.mu.Unlock()
+	//rf.mu.Unlock()
 
 	fmt.Println("passed first locks ")
 
