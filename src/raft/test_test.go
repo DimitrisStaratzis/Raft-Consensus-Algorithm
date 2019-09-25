9b08b62090c8cd244374c9d156cc25b5125de9a9
dimitrisstaratzis <jstaratzi@icloud.com>
2019-09-25 15:34:24 -0400

    initial request vote

--- src/raft/test_test.go
+++ src/raft/test_test.go
@@ -52,11 +52,13 @@ func TestReElection2A(t *testing.T) {
 	// if the leader disconnects, a new one should be elected.
 	cfg.disconnect(leader1)
 	cfg.checkOneLeader()
+	fmt.Printf("  ... Passed1\n")
 
 	// if the old leader rejoins, that shouldn't
 	// disturb the old leader.
 	cfg.connect(leader1)
 	leader2 := cfg.checkOneLeader()
+	fmt.Printf("  ... Passed2\n")
 
 	// if there's no quorum, no leader should
 	// be elected.
@@ -64,10 +66,12 @@ func TestReElection2A(t *testing.T) {
 	cfg.disconnect((leader2 + 1) % servers)
 	time.Sleep(2 * RaftElectionTimeout)
 	cfg.checkNoLeader()
+	fmt.Printf("  ... Passed3\n")
 
 	// if a quorum arises, it should elect a leader.
 	cfg.connect((leader2 + 1) % servers)
 	cfg.checkOneLeader()
+	fmt.Printf("  ... Passe4\n")
 
 	// re-join of last node shouldn't prevent leader from existing.
 	cfg.connect(leader2)
