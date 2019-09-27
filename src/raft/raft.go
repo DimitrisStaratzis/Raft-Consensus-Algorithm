3af0c89d1c0965417131f90015848e6f047dfe7b
dimitrisstaratzis <jstaratzi@icloud.com>
2019-09-27 00:23:10 -0400

    experiments

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -309,7 +309,7 @@ func (rf *Raft) startServer() {
 				rf.state = 0
 				rf.mu.Unlock()
 				//fmt.Println("HMOUN CANDIDATE KAI MOU IRTHE LEADER")
-			} else if (time.Now().UnixNano()/int64(time.Millisecond) - rf.electionStarted) > 350 {
+			} else if (time.Now().UnixNano()/int64(time.Millisecond) - rf.electionStarted) > 700 {
 				rf.mu.Lock()
 				rf.electionStarted = time.Now().UnixNano() / int64(time.Millisecond)
 				rf.currentTerm++
